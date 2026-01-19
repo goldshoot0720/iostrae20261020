43cd4d8e0c74b3f9a0e22559503c44e9c70386dc
goldshoot0720 <goldshoot0720@gmail.com>
2026-01-20 00:04:03 +0800

    Initial Commit

---
+++ iostrae20260120/iostrae20260120App.swift
@@ -0,0 +1,17 @@
+//
+//  iostrae20260120App.swift
+//  iostrae20260120
+//
+//  Created by 鋒兄 on 2026/1/20.
+//
+
+import SwiftUI
+
+@main
+struct iostrae20260120App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
